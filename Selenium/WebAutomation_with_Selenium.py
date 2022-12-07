744297228db5f4a3e24a3de944524fcdf5b2de48
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-06 23:13:54 -0300

    Update WebAutomation_with_Selenium.py

--- Selenium/WebAutomation_with_Selenium.py
+++ Selenium/WebAutomation_with_Selenium.py
@@ -1,11 +1,16 @@
 # Import Libraries
 
-import selenium
-import webdriver_manager # allows to create a browser controlled by selenium
+#import selenium
+#import webdriver_manager # allows to create a browser controlled by selenium
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service # Play ChromeDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service # Play ChromeDriverManager
 
-browser = webdriver.Chrome()
+# Identifies the current browser version and install ChromeDriverManager
+service = Service(ChromeDriverManager().install())
+
+browser = webdriver.Chrome(service=service)
 # For perfect operation it is necessary to download the web driver corresponding to the chosen browser
+
+browser.get("https://pages.hashtagtreinamentos.com/arquivo-python-1jGh7kZSxQLoznA_GgwnWa8f7LEl3kKCZ?origemurl=hashtag_yt_org_planilhapyt_8AMNaVt0z_M")
