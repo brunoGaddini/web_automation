ce6504f58005f3a0cbbc3b299691ce5c86c356fc
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-07 21:04:02 -0300

    Update WebAutomation_with_Selenium.py

--- Selenium/WebAutomation_with_Selenium.py
+++ Selenium/WebAutomation_with_Selenium.py
@@ -7,12 +7,25 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service # Play ChromeDriverManager
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 # Identifies the current browser version and install ChromeDriverManager
 service = Service(ChromeDriverManager().install())
 
 browser = webdriver.Chrome(service=service)
 # For perfect operation it is necessary to download the web driver corresponding to the chosen browser
 
-browser.get("https://pages.hashtagtreinamentos.com/arquivo-python-1jGh7kZSxQLoznA_GgwnWa8f7LEl3kKCZ?origemurl=hashtag_yt_org_planilhapyt_8AMNaVt0z_M")
+# Accessing the address to carry out the automation process
+browser.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M")
+# Setting browser read wait with some conditions
+try:
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    )
+finally:
+    browser.quit()
 
-#https://www.youtube.com/watch?v=8AMNaVt0z_M 18:24
+# Searching for an element within the page
+browser.find_element()
